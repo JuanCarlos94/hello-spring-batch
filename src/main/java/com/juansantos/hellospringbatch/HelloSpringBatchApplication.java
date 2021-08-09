1fd25a3f39c6e03b1de4ffe3b90836a12013a956
JuanCarlos94 <juancarloslsantos94@gmail.com>
2021-08-09 08:29:56 -0300

    Implementando uma simples chunk configuration

--- src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
+++ src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
@@ -2,24 +2,22 @@ package com.juansantos.hellospringbatch;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
-import org.springframework.batch.item.file.mapping.PassThroughLineMapper;
-import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -33,36 +31,37 @@ public class HelloSpringBatchApplication {
 
 	@Bean
 	public Job job(){
-		return this.jobBuilderFactory.get("job")
+		return this.jobBuilderFactory.get("chunkBasedJob")
 			.start(step1())
 			.build();
 	}
 
 	@Bean
 	public Step step1(){
-		return this.stepBuilderFactory.get("step1")
-			.<String, String>chunk(10)
-			.reader(itemReader(null))
-			.writer(itemWriter(null))
+		return this.stepBuilderFactory.get("chunkStep")
+			.<String, String>chunk(1000)
+			.reader(itemReader())
+			.writer(itemWriter())
 			.build();
 	}
 
 	@Bean
 	@StepScope
-	public FlatFileItemReader<String> itemReader(@Value("#{jobParameters['inputFile']}") Resource inputFile){
-		return new FlatFileItemReaderBuilder<String>().name("itemReader")
-			.resource(inputFile)
-			.lineMapper(new PassThroughLineMapper())
-			.build();
+	public ListItemReader<String> itemReader(){
+		List<String> items = new ArrayList<>(100000);
+		for(int i=0;i<100000;i++){
+			items.add(UUID.randomUUID().toString());
+		}
+		return new ListItemReader<>(items);
 	}
 
 	@Bean
-	@StepScope
-	public FlatFileItemWriter<String> itemWriter(@Value("#{jobParameters['outputFile']}") Resource outputFile){
-		return new FlatFileItemWriterBuilder<String>().name("itemWriter")
-			.resource(outputFile)
-			.lineAggregator(new PassThroughLineAggregator<>())
-			.build();
+	public ItemWriter<String> itemWriter(){
+		return items -> {
+			for(String item : items){
+				System.out.println(">> current item = " + item);
+			}
+		};
 	}
 
 	public static void main(String[] args) {
