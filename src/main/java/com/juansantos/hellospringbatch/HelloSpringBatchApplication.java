1277d94e1f543d93a62fa4c4f1945ac27ed6acfb
JuanCarlos94 <juancarloslsantos94@gmail.com>
2021-08-09 10:35:44 -0300

    Implementing Step Flow Conditional Logic

--- src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
+++ src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
@@ -2,27 +2,14 @@ package com.juansantos.hellospringbatch;
 
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.JobExecutionNotRunningException;
-import org.springframework.batch.core.launch.NoSuchJobException;
-import org.springframework.batch.core.launch.NoSuchJobExecutionException;
-import org.springframework.batch.core.launch.support.SimpleJobOperator;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -39,44 +26,65 @@ public class HelloSpringBatchApplication {
 	private StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job job(){
-		return this.jobBuilderFactory.get("chunkBasedJob")
-			.start(step1())
-			.build();
-	}
-
-	@Bean
-	public Step step1(){
-		return this.stepBuilderFactory.get("chunkStep")
-			.<String, String>chunk(randomCompletionPolicy())
-			.reader(itemReader())
-			.writer(itemWriter())
-			.listener(new LoggingStepStartStopListener())
-			.build();
-	}
-
-	@Bean
-	@StepScope
-	public ListItemReader<String> itemReader(){
-		List<String> items = new ArrayList<>(100000);
-		for(int i=0;i<100000;i++){
-			items.add(UUID.randomUUID().toString());
-		}
-		return new ListItemReader<>(items);
-	}
-
-	@Bean
-	public ItemWriter<String> itemWriter(){
-		return items -> {
-			for(String item : items){
-				System.out.println(">> current item = " + item);
-			}
+	public Tasklet passTasklet(){
+		return (contribution, chunkContext) -> {
+			return RepeatStatus.FINISHED;
 		};
 	}
 
 	@Bean
-	public CompletionPolicy randomCompletionPolicy(){
-		return new RandomChunkSizePolicy();
+	public Tasklet successTasklet(){
+		return (contribution, context) -> {
+			System.out.println("Success!");
+			return RepeatStatus.FINISHED;
+		};
+	}
+
+	@Bean
+	public Tasklet failTasklet(){
+		return (contribution, context) -> {
+			System.out.println("Failure!");
+			return RepeatStatus.FINISHED;
+		};
+	}
+
+	@Bean
+	public Job job(){
+		return this.jobBuilderFactory.get("conditionalJob")
+			.start(firstStep())
+			.next(decider())
+			.from(decider())
+			.on("FAILED").to(failureStep())
+			.from(decider())
+			.on("*").to(successStep())
+			.end()
+			.build();
+	}
+
+	@Bean
+	public Step firstStep(){
+		return this.stepBuilderFactory.get("firstStep")
+			.tasklet(passTasklet())
+			.build();
+	}
+
+	@Bean
+	public Step successStep(){
+		return this.stepBuilderFactory.get("successStep")
+			.tasklet(successTasklet())
+			.build();
+	}
+
+	@Bean
+	public Step failureStep(){
+		return this.stepBuilderFactory.get("failureStep")
+			.tasklet(failTasklet())
+			.build();
+	}
+
+	@Bean
+	public JobExecutionDecider decider(){
+		return new RandomDecider();
 	}
 
 	public static void main(String[] args) {
