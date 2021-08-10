7e34cfcb8b89a175f587bec31dfe319437fc218b
JuanCarlos94 <juancarloslsantos94@gmail.com>
2021-08-10 11:23:51 -0300

    Adicionado integração Quartz scheduler

--- src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
+++ src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
@@ -1,33 +1,22 @@
 package com.juansantos.hellospringbatch;
 
 
-import java.util.Properties;
-
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class HelloSpringBatchApplication {
-	
+
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
 
@@ -46,55 +35,10 @@ public class HelloSpringBatchApplication {
 	public Step step1(){
 		return this.stepBuilderFactory.get("step1")
 			.tasklet((stepContribution, chunkContext) -> {
-				System.out.println("step1 ran today!");
+				System.out.println("step1 ran!");
 				return RepeatStatus.FINISHED;
-			}).build();
-	}
-
-	@RestController
-	public static class JobLaunchingController {
-		@Autowired
-		private JobLauncher jobLauncher;
-
-		@Autowired
-		private ApplicationContext context;
-
-		@Autowired
-		private JobExplorer jobExplorer;
-		
-		@PostMapping(path="/run")
-		public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws Exception {
-			Job job = this.context.getBean(request.getName(), Job.class);
-			JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(), this.jobExplorer)
-				.getNextJobParameters(job)
-				.toJobParameters();
-			return this.jobLauncher.run(job, jobParameters).getExitStatus();
-		}
-	}
-
-	public static class JobLaunchRequest {
-		private String name;
-
-		private Properties jobParameters;
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public JobParameters getJobParameters() {
-			Properties properties = new Properties();
-			properties.putAll(this.jobParameters);
-			return new JobParametersBuilder(properties).toJobParameters();
-		}
-
-		public void setJobParameters(Properties jobParameters) {
-			this.jobParameters = jobParameters;
-		}
-		
+			})
+			.build();
 	}
 
 	public static void main(String[] args) {
