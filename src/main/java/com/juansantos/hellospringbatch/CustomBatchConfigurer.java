78a10b323df053bb9cb3390f061ebcd52a824236
JuanCarlos94 <juancarloslsantos94@gmail.com>
2021-08-10 08:04:46 -0300

    Listing 5-4. Customizing the JobExplorer

--- src/main/java/com/juansantos/hellospringbatch/CustomBatchConfigurer.java
+++ src/main/java/com/juansantos/hellospringbatch/CustomBatchConfigurer.java
@@ -3,9 +3,8 @@ package com.juansantos.hellospringbatch;
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
-import org.springframework.batch.support.DatabaseType;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -21,14 +20,10 @@ public class CustomBatchConfigurer extends DefaultBatchConfigurer {
     private PlatformTransactionManager transactionManager;
 
     @Override
-    protected JobRepository createJobRepository() throws Exception {
-        JobRepositoryFactoryBean factoryBean = new JobRepositoryFactoryBean();
-
-        factoryBean.setDatabaseType(DatabaseType.MYSQL.getProductName());
-        factoryBean.setTablePrefix("FOO_");
-        factoryBean.setIsolationLevelForCreate("ISOLATION_REPEATABLE_READ");
+    protected JobExplorer createJobExplorer() throws Exception {
+        JobExplorerFactoryBean factoryBean = new JobExplorerFactoryBean();
         factoryBean.setDataSource(this.dataSource);
-
+        factoryBean.setTablePrefix("FOO_");
         factoryBean.afterPropertiesSet();
         return factoryBean.getObject();
     }
