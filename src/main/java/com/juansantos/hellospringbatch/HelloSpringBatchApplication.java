55c86c877e5673330b048c9ff8fd4c97a55f370f
JuanCarlos94 <juancarloslsantos94@gmail.com>
2021-08-13 11:18:21 -0300

    Read xml example

--- src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
+++ src/main/java/com/juansantos/hellospringbatch/HelloSpringBatchApplication.java
@@ -1,18 +1,23 @@
 package com.juansantos.hellospringbatch;
 
 
-
-
-
 import com.juansantos.hellospringbatch.models.Customer;
+import com.juansantos.hellospringbatch.models.Transaction;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -20,20 +25,57 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @EnableBatchProcessing
 @SpringBootApplication
-public class HelloSpringBatchApplication implements CommandLineRunner {
+public class HelloSpringBatchApplication implements CommandLineRunner{
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Step copyFileStep(){
+		return this.stepBuilderFactory.get("copyFileStep")
+			.<Customer, Customer>chunk(10)
+			.reader(costomerFileReader(null))
+			.writer(itemWriter())
+			.build();
+	}
+
+	@Bean
+	public Job job(){
+		return this.jobBuilderFactory.get("job")
+			.incrementer(new RunIdIncrementer())
+			.start(copyFileStep())
+			.build();
+	}
 
 	@Bean
 	@StepScope
-	public FlatFileItemReader<Customer> customerItemReader(@Value("#{jobParameters['customerFile']}") Resource inputFile){
-		return new FlatFileItemReaderBuilder<Customer>()
-			.name("customerItemReader")
-			.lineTokenizer(new CustomerFileLineTokenizer())
-			.fieldSetMapper(new CustomerFieldSetMapper())
+	public StaxEventItemReader<Customer> costomerFileReader(@Value("#{jobParameters['customerFile']}") Resource inputFile){
+		System.out.println("Resource: " + inputFile);
+		return new StaxEventItemReaderBuilder<Customer>()
+			.name("customerFileReader")
 			.resource(inputFile)
+			.addFragmentRootElements("customer")
+			.unmarshaller(customerMarshaller())
 			.build();
+	}
+
+	@Bean
+	public Jaxb2Marshaller customerMarshaller(){
+		Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+		jaxb2Marshaller.setClassesToBeBound(Customer.class, Transaction.class);
+		return jaxb2Marshaller;
+	}
+
+	@Bean
+	public ItemWriter<Customer> itemWriter(){
+		return (items) -> items.forEach(System.out::println);
 	}
 	
 	public static void main(String[] args) {
@@ -43,12 +85,16 @@ public class HelloSpringBatchApplication implements CommandLineRunner {
 	@Autowired
 	private JobLauncher jobLauncher;
 
+	@Autowired
+	private JobExplorer jobExplorer;
+	
 	@Override
-	public void run(String... args){
-		JobParameters jobParameters = new JobParametersBuilder()
-			.addString("inputFile", "customers.csv")
+	public void run(String... args) throws Exception {
+		JobParameters params = new JobParametersBuilder(jobExplorer)
+			.addString("customerFile", "customer.xml")
+			.getNextJobParameters(job())
 			.toJobParameters();
-		jobLauncher.run(job(), jobParameters);
+		jobLauncher.run(job(), params);
 	}
 
 }
--- src/main/java/com/juansantos/hellospringbatch/models/Customer.java
+++ src/main/java/com/juansantos/hellospringbatch/models/Customer.java
@@ -1,7 +1,14 @@
 package com.juansantos.hellospringbatch.models;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="customer")
 public class Customer {
-    
+
     private String firstName;
     private String middleInitial;
     private String lastName;
@@ -10,11 +17,13 @@ public class Customer {
     private String state;
     private String zipCode;
 
+    private List<Transaction> transactions;
+
     public Customer() {
     }
 
-    public Customer(String firstName, String middleInitial, String lastName, String address,
-            String city, String state, String zipCode) {
+    public Customer(String firstName, String middleInitial, String lastName, String address, String city, String state,
+            String zipCode) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
         this.lastName = lastName;
@@ -23,59 +32,88 @@ public class Customer {
         this.state = state;
         this.zipCode = zipCode;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getMiddleInitial() {
         return middleInitial;
     }
+
     public void setMiddleInitial(String middleInitial) {
         this.middleInitial = middleInitial;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public String getZipCode() {
         return zipCode;
     }
+
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    @XmlElementWrapper(name = "transactions")
+    @XmlElement(name = "transaction")
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
     @Override
     public String toString() {
-        return "Customer{" +
-            "firstName='" + firstName + "'" +
-            ", middleInitial ='" + middleInitial + "'" +
-            ", lastName='" + lastName + "'" +
-            ", address='" + address + "'" +
-            ", city='" + city + "'" +
-            ", state='" + state + "'" +
-            ", zipCode='" + zipCode + "'" +
-            "}";
+        StringBuilder output = new StringBuilder();
+        output.append(firstName);
+        output.append(" ");
+        output.append(middleInitial);
+        output.append(". ");
+        output.append(lastName);
+        if (transactions != null && transactions.size() > 0) {
+            output.append(" has ");
+            output.append(transactions.size());
+            output.append(" transactions.");
+        } else {
+            output.append(" has no transactions.");
+        }
+        return output.toString();
     }
 }
